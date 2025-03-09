9857964a68e681f589b5229dd980653d86808ce5
d1pankarmedhi <dipankarmedhi11@gmail.com>
2025-03-10 00:32:24 +0530

    show experience before skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Experience from './components/Experience';
-import Projects from './components/Projects';
-import Blog from './components/Blog';
-import Publications from './components/Publications';
-import Footer from './components/Footer';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Blog from "./components/Blog";
+import Publications from "./components/Publications";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
       <Hero />
-      <About />
       <Experience />
+      <About />
       <Projects />
       <Blog />
       <Publications />
